620a2d8a6ef4576d51842114d21880bdcc3372fe
smeth194243 <smor7432@740s-pl-114.local>
2016-11-01 12:31:14 -0600

    Added window title.

--- src/graph/view/FirstFrame.java
+++ src/graph/view/FirstFrame.java
@@ -7,17 +7,20 @@ import graph.controller.GraphController;
 public class FirstFrame extends JFrame
 {
 	private GraphController baseController;
+	private FirstPanel appPanel;
 	
 	public FirstFrame(GraphController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
+		this.appPanel = new FirstPanel(baseController);
 		this.setupFrame();
 	}
 	
 	public void setupFrame()
 	{
+		this.setContentPane(appPanel);
+		this.setTitle("OMG - WINDOWS!!!");
 		this.setSize(new Dimension(600,400));
 		this.setLocationRelativeTo(null);
 		this. setVisible(true);

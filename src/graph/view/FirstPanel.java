0895ccdba6e9d55ffe56e49dd864477c53b152ae
smeth194243 <smor7432@740s-pl-114.local>
2016-11-01 12:27:12 -0600

    same

--- src/graph/view/FirstPanel.java
+++ src/graph/view/FirstPanel.java
@@ -1,6 +1,34 @@
 package graph.view;
 
-public class FirstPanel
-{
+import javax.swing.JPanel;
+import graph.controller.GraphController;
 
+public class FirstPanel extends JPanel
+{
+	private GraphController baseController;
+	
+	public FirstPanel(GraphController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }

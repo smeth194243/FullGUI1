23629e3c2895269e49d02886fc24853a3688cb25
smeth194243 <smor7432@740s-pl-114.local>
2016-11-01 11:51:08 -0600

    Created project.

---
+++ src/graph/controller/GraphController.java
@@ -0,0 +1,16 @@
+package graph.controller;
+
+public class GraphController
+{
+	public GraphController()
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+	}
+	
+	
+}
---
+++ src/graph/controller/Runner.java
@@ -0,0 +1,10 @@
+package graph.controller;
+
+public class Runner
+{
+	public static void main(String[]args)
+	{
+		GraphController graphAppController = new GraphController();
+		graphAppController.start();
+	}
+}

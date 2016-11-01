661294536e1503d15a5b3662ac2baab9cbecf74c
smeth194243 <smor7432@740s-pl-114.local>
2016-11-01 12:52:04 -0600

    Added button things

--- src/graph/view/FirstPanel.java
+++ src/graph/view/FirstPanel.java
@@ -1,17 +1,23 @@
 package graph.view;
 
 import javax.swing.JPanel;
+import java.awt.Color;
 import graph.controller.GraphController;
+import javax.swing.JButton;
 
 public class FirstPanel extends JPanel
 {
 	private GraphController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	public FirstPanel(GraphController baseController)
 	{
 		super();
 		this.baseController = baseController;
 		
+		this.colorButton= new JButton ("colors!");
+		this.randomButton = new JButton("random colors!!");
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -19,7 +25,9 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setBackground(Color.CYAN);
+		this.add(colorButton);
+		this.add(randomButton);
 	}
 	
 	private void setupLayout()

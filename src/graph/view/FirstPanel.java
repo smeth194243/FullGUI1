7794fb0a7f85302242ae76e68bfe1f89e85c7a82
smeth194243 <smor7432@740s-pl-114.local>
2016-11-01 13:42:39 -0600

    finalized gui

--- src/graph/view/FirstPanel.java
+++ src/graph/view/FirstPanel.java
@@ -1,13 +1,13 @@
 package graph.view;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.Color;
-import javax.swing.SpringLayout;
 import graph.controller.GraphController;
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstPanel extends JPanel
 {
@@ -15,31 +15,29 @@ public class FirstPanel extends JPanel
 	private JButton colorButton;
 	private JButton randomButton;
 	private SpringLayout baseLayout;
-	
+
 	public FirstPanel(GraphController baseController)
 	{
 		super();
 		this.baseController = baseController;
 		this.baseLayout = new SpringLayout();
-		this.colorButton= new JButton ("colors!");
+		this.colorButton = new JButton("colors!");
+
 		colorButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
-		baseLayout.putConstraint(SpringLayout.NORTH, colorButton, 0, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.WEST, colorButton, 0, SpringLayout.WEST, this);
-		colorButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
 			}
 		});
 		this.randomButton = new JButton("random colors!!");
-		baseLayout.putConstraint(SpringLayout.NORTH, randomButton, 89, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.WEST, randomButton, 158, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, randomButton, -119, SpringLayout.SOUTH, this);
-		baseLayout.putConstraint(SpringLayout.EAST, randomButton, -98, SpringLayout.EAST, this);
+
 		randomButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
-	
+
 	private void setupPanel()
 	{
 		this.setLayout(baseLayout);
@@ -47,14 +45,42 @@ public class FirstPanel extends JPanel
 		this.add(colorButton);
 		this.add(randomButton);
 	}
-	
+
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.EAST, colorButton, -166, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, colorButton, -21, SpringLayout.NORTH, randomButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, randomButton, 100, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, randomButton, 150, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, randomButton, -108, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, randomButton, -106, SpringLayout.EAST, this);
 	}
-	
+
 	private void setupListeners()
 	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.MAGENTA);
+			}
+		});
 		
+		randomButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeBackgroundColor();
+			}
+		});
 	}
+
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		this.setBackground(new Color(red, green, blue));
+	}
+
 }

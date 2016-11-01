2a2870b8b268e3f97eb82b7780e67ddaea7dd361
smeth194243 <smor7432@740s-pl-114.local>
2016-11-01 12:07:08 -0600

    Added FirstFrame to graph.controller

--- src/graph/controller/GraphController.java
+++ src/graph/controller/GraphController.java
@@ -1,10 +1,14 @@
 package graph.controller;
 
+import graph.view.FirstFrame;
+
 public class GraphController
 {
+	private FirstFrame appFrame;
+	
 	public GraphController()
 	{
-		
+		this.appFrame = new FirstFrame(this);
 	}
 	
 	public void start()
